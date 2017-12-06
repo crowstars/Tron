d8130d166367b99ddf7538bdd062b7a4b0794f12
crowstars <32338243+crowstars@users.noreply.github.com>
2017-12-06 10:04:31 -0800

    commented out parts

--- src/TronModel.java
+++ src/TronModel.java
@@ -63,18 +63,18 @@ public class TronModel {
 
 	public void setPlayer1Direction(int direction){
 		int currentDirection = player1.getDirection();
-		if(!((currentDirection == NORTH && direction == SOUTH) || (currentDirection == SOUTH && direction == NORTH)
-				|| (currentDirection == EAST && direction == WEST) || (currentDirection == WEST && direction == EAST))){
+		//if(!((currentDirection == NORTH && direction == SOUTH) || (currentDirection == SOUTH && direction == NORTH)
+				//|| (currentDirection == EAST && direction == WEST) || (currentDirection == WEST && direction == EAST))){
 		player1.setDirection(direction);
-		}
+		//}
 	}
 
 	public void setPlayer2Direction(int direction){
 		int currentDirection = player1.getDirection();
-		if(!((currentDirection == NORTH && direction == SOUTH) || (currentDirection == SOUTH && direction == NORTH)
-				|| (currentDirection == EAST && direction == WEST) || (currentDirection == WEST && direction == EAST))){
+		//if(!((currentDirection == NORTH && direction == SOUTH) || (currentDirection == SOUTH && direction == NORTH)
+				//|| (currentDirection == EAST && direction == WEST) || (currentDirection == WEST && direction == EAST))){
 			player2.setDirection(direction);
-		}
+		//}
 	}
 
 	public void movePlayers(){
