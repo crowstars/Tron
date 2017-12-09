73e9de4ca65f7ab9d64bac26c00a0ea2763a2d6f
Quinn Vinlove <quinnvinlove@skylab.org>
2017-12-09 15:07:28 -0800

    Added game difficulty options
    Added ability to choose between three game difficulty options: Easy, Medium, and Hard. Changed overall game control structure to accomidate extra setup steps like these at the begenning.

--- src/Tron.java
+++ src/Tron.java
@@ -1,8 +1,10 @@
+
 /*WISH LIST
- * head on collision detection for winners (change in model)
- * No option for suicide (turning in on yourslef)
- * GUI for speed (easyness) at begenning 
- * Sprites for players
+ * fix head on collision mode1
+ * 
+ *WORKING ON>>>>
+ *GUIs for setup (gameSpeed and color)
+ * 
  */
 
 import java.awt.Color;
@@ -19,18 +21,19 @@ public class Tron {
 
 	public TronModel model;
 
-	public final int SPEED = 50; // speed of game clock.
 	public final int BOARD_SIZE = 40; // size of board
-	
+
 	public int player1Points = 0;
 	public int player2Points = 0;
+
+	public int gameSpeed = 50;
 
 	public static void main(String args[]) {
 		new Tron().runGame();
 	}
 
 	public void drawBasic() {
-		
+
 		// Color window
 		StdDraw.setPenColor(StdDraw.PRINCETON_ORANGE);
 		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.5);
@@ -48,7 +51,44 @@ public class Tron {
 		// set title text
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.text(0.5, 0.95, "Tron!");
+	}
 
+	public void difficultySelect() {
+		clearInfoDisplay();
+
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.text(0.5, 0.15, "Select Difficulty");
+
+		StdDraw.text(0.2775, 0.1, "Easy");
+		StdDraw.text(0.5, 0.1, "Medium");
+		StdDraw.text(0.7225, 0.1, "Hard");
+
+		// wait for click in three regions
+		while (!StdDraw.isMousePressed()) {
+		}
+
+		// calculate mouse ranges, if within bounds, continue
+		if (StdDraw.mouseY() < 0.18 && StdDraw.mouseY() > 0.02 && StdDraw.mouseX() > 0.055
+				&& StdDraw.mouseX() < 0.945) {
+			if (StdDraw.mouseX() < 0.4255) {
+				System.out.println("EASY");
+				gameSpeed = 80;
+			} else if (StdDraw.mouseX() < 0.648) {
+				System.out.println("MEDIUM");
+				gameSpeed = 50;
+			} else {
+				System.out.println("HARD");
+				gameSpeed = 30;
+			}
+		} else {
+			difficultySelect();
+		}
+	}
+
+	public void drawTutorial() {
+		clearInfoDisplay();
+
+		StdDraw.setPenColor(StdDraw.BLACK);
 		// draw tutorial text
 		StdDraw.text(0.25, 0.15, "Player 1");
 		StdDraw.text(0.25, 0.1, "Steer with WASD");
@@ -57,12 +97,17 @@ public class Tron {
 		StdDraw.text(0.75, 0.1, "Steer with IJKL");
 
 		StdDraw.text(0.5, 0.05, "SPACE to start");
-		
+
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.text(0.25, 0.05, "Points: " + player1Points);
 		StdDraw.text(0.75, 0.05, "Points: " + player2Points);
-		
-		StdDraw.enableDoubleBuffering();
+	}
+
+	public void clearInfoDisplay() {
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.filledRectangle(0.5, 0.10, 0.445, 0.085);
+		StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
+		StdDraw.filledRectangle(0.5, 0.10, 0.44, 0.08);
 	}
 
 	public void handleKeyPresses() {
@@ -115,48 +160,54 @@ public class Tron {
 
 		StdDraw.text(0.5, 0.08, "Press SPACE to play again");
 	}
-	
+
 	public void showBoard() {
 		Color[][] display = model.getBoard();
 
 		for (int i = 0; i < display.length; i++) {
 			for (int j = 0; j < display.length; j++) {
-				
+
 				double pieceSize = 0.35 / display.length;
 
-				double xLocation = (j * pieceSize * 2) + 0.15;//change this
-				double yLocation = (i * pieceSize * 2) + 0.21;//change this
+				double xLocation = (j * pieceSize * 2) + 0.15;// change this
+				double yLocation = (i * pieceSize * 2) + 0.21;// change this
 
 				if (display[i][j] != null) {
 					if (display[i][j].equals(Color.YELLOW)) {
 						StdDraw.setPenColor(player1Color);
-						StdDraw.filledRectangle(xLocation, yLocation, pieceSize, pieceSize); //change this
+						StdDraw.filledRectangle(xLocation, yLocation, pieceSize, pieceSize); // change
+																								// this
 					}
 
 					else if (display[i][j].equals(Color.BLUE)) {
 						StdDraw.setPenColor(player2Color);
-						StdDraw.filledRectangle(xLocation, yLocation, pieceSize, pieceSize);//change this
+						StdDraw.filledRectangle(xLocation, yLocation, pieceSize, pieceSize);// change
+																							// this
 					}
 				}
-			}	
+			}
 		}
-		
-}
+
+	}
 
 	// dynamically redraws board to be faster
 	public void showBoardFast() {
-		
+
 		double pieceSize = 0.35 / BOARD_SIZE;
-		double player1X = (model.getPlayer1().getX() * pieceSize * 2) + 0.15;// change this
-		double player1Y = (model.getPlayer1().getY() * pieceSize * 2) + 0.21;// change this
-		double player2X = (model.getPlayer2().getX() * pieceSize * 2) + 0.15;// change this
-		double player2Y = (model.getPlayer2().getY() * pieceSize * 2) + 0.21;// change this
-		
+		double player1X = (model.getPlayer1().getX() * pieceSize * 2) + 0.15;// change
+																				// this
+		double player1Y = (model.getPlayer1().getY() * pieceSize * 2) + 0.21;// change
+																				// this
+		double player2X = (model.getPlayer2().getX() * pieceSize * 2) + 0.15;// change
+																				// this
+		double player2Y = (model.getPlayer2().getY() * pieceSize * 2) + 0.21;// change
+																				// this
+
 		StdDraw.setPenColor(player1Color);
-		StdDraw.filledRectangle(player1X, player1Y, pieceSize, pieceSize); 
+		StdDraw.filledRectangle(player1X, player1Y, pieceSize, pieceSize);
 
 		StdDraw.setPenColor(player2Color);
-		StdDraw.filledRectangle(player2X, player2Y, pieceSize, pieceSize);																			
+		StdDraw.filledRectangle(player2X, player2Y, pieceSize, pieceSize);
 		StdDraw.show();
 	}
 
@@ -170,19 +221,27 @@ public class Tron {
 		model = new TronModel(BOARD_SIZE);
 
 		drawBasic();
+		difficultySelect();
+		drawTutorial();
 		waitForSpace();
 
-		while (!model.isGameOver()) {
-			handleKeyPresses();
-			showBoardFast();
-			model.movePlayers();
-			StdDraw.pause(SPEED); // slow game clock for testing!
-			System.out.println("clock");
+		while (true) {
+			StdDraw.enableDoubleBuffering();
+			while (!model.isGameOver()) {
+				handleKeyPresses();
+				showBoardFast();
+				model.movePlayers();
+				StdDraw.pause(gameSpeed); // slow game clock for testing!
+				System.out.println("clock");
+			}
+
+			handleWinner();
+			waitForSpace(); 
+			
+			model = new TronModel(BOARD_SIZE);
+			drawBasic();
+			drawTutorial();
 		}
-
-		handleWinner();
-		waitForSpace();
-		runGame(); // run game again
 	}
 
 }
