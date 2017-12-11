c58f334260dc3b46a56b4fee4d3e2406046df4b2
kennytran2139 <kennytran@lclark.edu>
2017-12-11 09:41:39 -0800

    Added Javadoc comments to and formatted TronModel Class

--- src/TronModel.java
+++ src/TronModel.java
@@ -1,21 +1,28 @@
 import java.awt.Color;
 
-//the problem is here
+/** Tron Model */
 public class TronModel {
+	
+	/** Board color array */
 	private Color[][] board;
 
+	/** Player variables */
 	private Player player1;
 	private Player player2;
 
+	/** Game over variable */
 	boolean gameOver;
+	
+	/** Winner variable */
 	Color winner;
 
+	/** Direction variables */
 	public final int NORTH = 0;
 	public final int EAST = 1;
 	public final int SOUTH = 2;
 	public final int WEST = 3;
 
-	// Creates the board and sets the starting position for the players
+	/** Creates the board and sets the starting position for the players */
 	public TronModel(int width) {
 		board = new Color[width][width];
 		
@@ -25,22 +32,22 @@ public class TronModel {
 		
 		player1 = new Player(StdDraw.YELLOW, player1Start, midpoint, EAST);
 		player2 = new Player(StdDraw.BLUE, player2Start, midpoint, WEST);
-		// updateBoard();
 		gameOver = false;
 		winner = null;
 	}
 
+	/** Updates the board */
 	public void updateBoard() {
 		board[player1.getX()][player1.getY()] = player1.getColor();
 		board[player2.getX()][player2.getY()] = player2.getColor();
 	}
 
-	// getter for board
+	/** Getter for board */
 	public Color[][] getBoard() {
 		return board;
 	}
 
-	// checks to see if location is empty
+	/** Checks to see if location is empty */
 	public boolean isEmpty(int x, int y) {
 		if (board[x][y] == null) {
 			return true;
@@ -48,45 +55,46 @@ public class TronModel {
 			return false;
 	}
 
-	// checks to see if game is over
+	/** Checks to see if game is over */
 	public boolean isGameOver() {
 		return gameOver;
 	}
 
-	// keep returning null if nobody has won yet, otherwise return player that has
-	// won
+	/** Color of winner */
 	public Color getWinner() {
 		return winner;
 	}
-
+	
+	/** Player 1 */
 	public Player getPlayer1() {
 		return player1;
 	}
-
+	
+	/** Player 2 */
 	public Player getPlayer2() {
 		return player2;
 	}
-
+	
+	/** Sets player 1 directions and prevents player suicide */
 	public void setPlayer1Direction(int direction) {
-		//stops player from committing suicide: any direction thats 2 away from the
-		//integer value of the current direction is the opposite of the current direction,
-		//and therefore illegal
 		if(Math.abs(direction - player1.getDirection()) != 2){
 			player1.setDirection(direction);
 		}
 	}
-
+	
+	/** Sets player 2 directions and prevents player suicide */
 	public void setPlayer2Direction(int direction) {
 		if(Math.abs(direction - player2.getDirection()) != 2){
 			player2.setDirection(direction);
 		}
 	}
 
+	/** Allows players to move on board and sets directions for movement */
 	public void movePlayers() {
 		// Find destinations
 		Location destination1;
 		Location destination2;
-
+		// Directions for player 1
 		int direction = player1.getDirection();
 		if (direction == WEST) {
 			destination1 = new Location(player1.getX() - 1, player1.getY());
@@ -99,7 +107,8 @@ public class TronModel {
 		else {
 			destination1 = new Location(player1.getX(), player1.getY() - 1);
 		}
-
+		
+		// Directions for player 2
 		direction = player2.getDirection();
 		if (direction == WEST) {
 			destination2 = new Location(player2.getX() - 1, player2.getY());
@@ -138,7 +147,7 @@ public class TronModel {
 			winner = player1.getColor();
 			System.out.println("Player 2 ran into light trail at " + destination2);
 		}
-
+		// Continues the game if game is not over
 		if (!gameOver) {
 			player1.setX(destination1.getY());
 			player1.setY(destination1.getX());
@@ -153,12 +162,12 @@ public class TronModel {
 		}
 	}
 
+	/** Gets the color of the winner */
 	public Color getWinnerColor() {
 		return winner;
 	}
-
-	//Accessors for tests
-	//Accessors and Modifiers for tests
+	
+	/** Accessors and Modifiers for tests */
 	public void setColor(int x, int y, Color color){
 		if(x<board.length && y<board.length) {
 			board[y][x] = color;
